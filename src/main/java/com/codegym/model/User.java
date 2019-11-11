3cae8b925261e5ede76d6916917ac8bc8c7ab8f7
TRANNHUHAI <tranhaiexp@gmail.com>
2019-11-11 15:31:40 +0700

    create model

--- src/main/java/com/codegym/model/User.java
+++ src/main/java/com/codegym/model/User.java
@@ -1,4 +1,75 @@
 package com.codegym.model;
 
 public class User {
+    private Long id;
+    private String email;
+    private String name;
+    private Integer phone;
+    private String Address;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(Long id, String email, String name, Integer phone, String address, String password) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+        this.phone = phone;
+        Address = address;
+        this.password = password;
+    }
 }
